ee057e0c6bb55aee0e5a1722370f7946a3c1af82
trucb1203981 <trucb1203981@gmail.com>
2018-10-01 20:13:58 +0700

    remove @babel\polifill

--- resources/nuxt/plugins/vuetify.js
+++ resources/nuxt/plugins/vuetify.js
@@ -1,7 +1,7 @@
 // import Vue from 'vue'
 // import Vuetify from 'vuetify'
 import 'babel-polyfill'
-import '@babel/polyfill'
+// import '@babel/polyfill'
 // import '@fortawesome/fontawesome-free/css/all.css'
 // Vue.use(Vuetify, {
 // 	iconfont: 'fa'

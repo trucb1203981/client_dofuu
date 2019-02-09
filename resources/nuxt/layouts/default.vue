45ddf8764c5d6e8e90cb7898b04e090670e843b5
trucb1203981 <trucb1203981@gmail.com>
2019-02-09 17:58:47 +0700

    remove dialog

--- resources/nuxt/layouts/default.vue
+++ resources/nuxt/layouts/default.vue
@@ -4,36 +4,6 @@
 		<vue-left-navigation class="hidden-md-and-up"></vue-left-navigation>
 		<v-content style="min-height: 400px">
 			<nuxt/>
-			<v-dialog v-model="dialog" max-width="500">
-				<v-card>
-					<v-toolbar dense color="grey lighten-4" class="elevation-0">
-						<v-avatar size="24px" tile>
-							<img src="~/static/dofuu24x24.png">
-						</v-avatar>
-						<v-toolbar-title>
-							Thông Báo Nghỉ Tết Xuân Kỷ Hợi 2019
-						</v-toolbar-title>
-					</v-toolbar>
-					<v-divider></v-divider>
-					<v-card-text class="text-xs-center">
-						<div class="body-2">
-							- Quý khách thân mến, công ty Dofuu xin phép được tạm nghỉ tết từ 30 tết (04/02/2019) đến hết mùng 5 (09/02/2019). Sau đó từ mùng 6 (10/02/2019) hoạt động trở lại bình thường.
-						</div>
-						<div class="body-2">
-							- Xin trân trọng cám ơn quý khách đã ủng hộ Dofuu trong suốt thời gian qua. 
-						</div>
-						<div class="body-2">
-							- Trong năm qua công ty phục vụ còn có nhiều thiếu sót, mong quý khách thông cảm. Dofuu sẽ cải thiện và nâng cao chất lượng phục vụ nhằm mang đến sự hài lòng cho quý khách.
-						</div>
-						<div class="body-2 red--text text--accent-3">	
-							Chúc quý khách "Một Năm Mới Vui Vẻ, Hạnh Phúc, An Khang Thịnh Vượng, Vạn Sự Như Ý".
-						</div>
-					</v-card-text>
-					<v-card-actions>
-						<v-btn block color="grey lighten-4" round small @click.native="dialog = false">Đóng</v-btn>
-					</v-card-actions>
-				</v-card>
-			</v-dialog>
 		</v-content>
 		<vue-footer />
 	</v-app>
@@ -55,7 +25,6 @@
 		},
 		data() {
 			return {
-				dialog: true
 			}
 		},
 		computed: {
